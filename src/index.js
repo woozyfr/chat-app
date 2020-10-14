81dea5b75c8f9a3ec3964b05cb04f85c02e025c4
woozyfr <alex@woozy.fr>
2020-10-14 09:29:40 +0200

    add exo

--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Contact from './components/Contact';
+import ContactList from './components/ContactList';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Contact name="Sara" avatar="https://randomuser.me/api/portraits/men/57.jpg"/>
-    <Contact name="Paul"  avatar="https://randomuser.me/api/portraits/men/54.jpg" isOnline/>
-    <Contact name="Djamila"  avatar="https://randomuser.me/api/portraits/men/52.jpg"/>
-    <Contact name="Bruno"  avatar="https://randomuser.me/api/portraits/men/51.jpg" isOnline/>
+    <ContactList/>
   </React.StrictMode>,
   document.getElementById('root')
 );
